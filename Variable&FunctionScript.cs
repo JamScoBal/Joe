d43e81413572718483ec611ace9cd7962faadf79
JB <10531760@uvu.edu>
2016-01-14 18:13:55 -0700

    no message

---
+++ Variable&FunctionScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	int Tom = 5;
+	void Start () {
+		Tom = Jerry (Tom);
+		Debug.Log (Tom);
+	
+	}
+	
+
+	int Jerry(int Mouse) {
+		int Cat;
+		Cat = Mouse - 10;
+		return Cat;
+	
+	}
+}
