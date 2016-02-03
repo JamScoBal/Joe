02204244fca4a9a6c8773ea9350771a77be2009c
JB <10531760@uvu.edu>
2016-02-02 18:21:54 -0700

    no message

---
+++ While.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class While : MonoBehaviour {
+	int Pup = 10;
+	// Use this for initialization
+	void Start () {
+		while (Pup >= 5) 
+		{
+			Debug.Log ("Dog");
+			Pup--;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
