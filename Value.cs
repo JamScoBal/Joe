f6edb51d2702e30abe0ca27aa18ebf64726f60ab
JB <10531760@uvu.edu>
2016-03-03 17:57:58 -0700

    no message

---
+++ Value.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Value : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int Jane = 5;
+		int Jack = Jane;
+		Jane = 15;
+
+		Debug.Log (Jack);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
