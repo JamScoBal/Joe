dfd50c5550b74aed57b036c88e2a058f2ee892b0
JB <10531760@uvu.edu>
2016-03-08 18:01:39 -0700

    no message

---
+++ ForArray.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForArray : MonoBehaviour {
+	string[] Salutations = {"So long", "Fair well", "Hasta La Vista"};
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i < Salutations.Length; i++) {
+			Debug.Log(Salutations[i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
